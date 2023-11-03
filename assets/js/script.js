e190e2177dad861e13241b5125cae977d420ae31
bitdern <acnewell09@gmail.com>
2023-11-03 14:53:30 -0500

    header and nav

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Add eventListener on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// PreLoader
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
