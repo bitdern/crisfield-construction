fee4d0d448f50a1ffbde785f5820a285cf4423fb
bitdern <acnewell09@gmail.com>
2023-11-08 08:55:26 -0600

    contact form email functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,23 @@ const activeHeader = function () {
 };
 
 window.addEventListener("scroll", activeHeader);
+
+// Opening up user's email client in contact form
+function sendEmail(event) {
+  event.preventDefault();
+
+  const fullName = document.querySelector(".full-name").value;
+  const emailAddress = document.querySelector(".email-address").value;
+  const mobileNumber = document.querySelector(".mobile-number").value;
+  const emailSubject = document.querySelector(".email-subject").value;
+  const emailBody = document.querySelector(".email-body").value;
+
+  // TEST FUNCTIONALITY W MY ADDY
+  // the variables customize the mailto link
+  const mailtoLink = `mailto:acnewell09@gmail.com?subject=${emailSubject}&body=${emailBody}`;
+
+  window.open(mailtoLink);
+
+  // reset the form & clear its contents after submission
+  event.target.reset();
+}
